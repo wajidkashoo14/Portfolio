9e6480d7e7c423eb60e2a713306452d3fb79d110
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2023-08-31 21:43:19 +0530

    added about us section

--- components/intro.tsx
+++ components/intro.tsx
@@ -44,7 +44,7 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -54,7 +54,7 @@ export default function Intro() {
         <span className="font-bold">1 year</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-      </motion.p>
+      </motion.h1>
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium "
         initial={{ opacity: 0 }}
@@ -72,7 +72,7 @@ export default function Intro() {
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
           href="/CV.pdf"
           download
         >
@@ -80,11 +80,11 @@ export default function Intro() {
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />{" "}
         </a>
 
-        <a className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full">
+        <a className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full hover:text-gray-950 focus:scale-110 hover:scale-[1.15] active:scale-[1.15] transition cursor-pointer border border-black/10" href="https://www.linkedin.com/in/wajid-kashoo-211046208" target="_blank">
           <BsLinkedin />
         </a>
 
-        <a className="bg-white p-4 flex text-gray-700 items-center gap-2 text=[1.35rem] rounded-full">
+        <a className="bg-white p-4 flex text-gray-700 items-center gap-2 text=[1.35rem] rounded-full hover:text-gray-950 focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href="https://github.com/wajidkashoo14" target="_blank" >
           <FaGitSquare />
         </a>
       </motion.div>
