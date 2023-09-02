996a0f9f7190294e955ba0752a21b4d74cf60b11
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2023-09-02 16:16:43 +0530

    added projects section

---
+++ components/projects.tsx
@@ -0,0 +1,19 @@
+import Project from "./project";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <Project key={index} {...project} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+
