16f765f459d5be5cd2d1911ce57ffa47aa41062d
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2023-09-10 15:19:10 +0530

    added hook

--- components/about.tsx
+++ components/about.tsx
@@ -1,21 +1,10 @@
 "use client";
-import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import { useActiveSectionContext } from "@/context/active-section-context";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function About() {
-  const { ref, inView } = useInView({
-    threshold:0.75
-  });
-  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
-
-  useEffect(() => {
-    if (inView && Date.now() - timeOfLastClick > 1000) {
-      setActiveSection("About");
-    }
-  }, [inView, setActiveSection, timeOfLastClick]);
+  const { ref } = useSectionInView("About")
   return (
     <motion.section
       ref={ref}
--- components/projects.tsx
+++ components/projects.tsx
@@ -3,21 +3,11 @@ import React, { useEffect } from "react";
 import Project from "./project";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
-import { useInView } from "react-intersection-observer";
-import { useActiveSectionContext } from "@/context/active-section-context";
+import { useSectionInView } from "@/lib/hooks";
 
 
 export default function Projects() {
-  const { ref, inView } = useInView({
-    threshold:0.5
-  });
-  const { setActiveSection, timeOfLastClick  } = useActiveSectionContext();
-
-  useEffect(() => {
-    if (inView && Date.now() - timeOfLastClick > 1000) {
-      setActiveSection("Projects");
-    }
-  }, [inView, setActiveSection, timeOfLastClick]);
+  const { ref} = useSectionInView("Projects")
   return (
     <section ref={ref} id="projects" className="scroll-mt-28">
       <SectionHeading>My projects</SectionHeading>
