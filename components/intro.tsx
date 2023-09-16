d1eaf75642a7a6c95b98ac9b13a4e7b977af5b5a
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2023-09-16 16:04:05 +0530

    added contact ui

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,12 +7,18 @@ import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGitSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
-  const { ref } = useSectionInView('Home', 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  const { ref } = useSectionInView("Home", 0.5);
 
   return (
-    <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -70,6 +76,10 @@ export default function Intro() {
         <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
         >
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />{" "}
@@ -84,11 +94,19 @@ export default function Intro() {
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />{" "}
         </a>
 
-        <a className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full hover:text-gray-950 focus:scale-110 hover:scale-[1.15] active:scale-[1.15] transition cursor-pointer border border-black/10" href="https://www.linkedin.com/in/wajid-kashoo-211046208" target="_blank">
+        <a
+          className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full hover:text-gray-950 focus:scale-110 hover:scale-[1.15] active:scale-[1.15] transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/wajid-kashoo-211046208"
+          target="_blank"
+        >
           <BsLinkedin />
         </a>
 
-        <a className="bg-white p-4 flex text-gray-700 items-center gap-2 text=[1.35rem] rounded-full hover:text-gray-950 focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href="https://github.com/wajidkashoo14" target="_blank" >
+        <a
+          className="bg-white p-4 flex text-gray-700 items-center gap-2 text=[1.35rem] rounded-full hover:text-gray-950 focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/wajidkashoo14"
+          target="_blank"
+        >
           <FaGitSquare />
         </a>
       </motion.div>
