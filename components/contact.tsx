66762341bf217633836bebef4e8e76aa84b15a07
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2023-10-09 20:44:32 +0530

    completed dark mode and responsiveness

--- components/contact.tsx
+++ components/contact.tsx
@@ -30,7 +30,7 @@ export default function Contact() {
       }}
     >
       <SectionHeading>Contact me</SectionHeading>
-      <p className="text-gray-700 -mt-6">
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:wajidkashoo14@gmail.com">
           wajidkashoo14@gmail.com
@@ -39,7 +39,7 @@ export default function Contact() {
       </p>
 
       <form
-        className="mt-10 flex flex-col"
+        className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
 
@@ -57,14 +57,14 @@ export default function Contact() {
           name="email"
           required
           maxLength={50}
-          className="h-14 rounded-lg borderBlack p-4"
+          className="h-14 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none "
         />
         <textarea
           name="message"
           required
           maxLength={500}
           placeholder="Your message"
-          className="h-52 my-3 rounded-lg borderBlack p-4"
+          className="h-52 my-3 rounded-lg borderBlack p-4  dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none "
         />
         <SubmitBtn />
       </form>
--- components/experience.tsx
+++ components/experience.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useContext } from "react";
 import SectionHeading from "./section-heading";
 import {
   VerticalTimeline,
@@ -8,10 +8,12 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
+import { useTheme } from "@/context/theme-context";
 
 export default function Experience() {
     const { ref } = useSectionInView("Experience");
-  return (
+    const {theme} = useTheme()
+    return (
     <section ref={ref} id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor="">
@@ -19,25 +21,25 @@ export default function Experience() {
           <React.Fragment key={index}>
             <VerticalTimelineElement
               contentStyle={{
-                backgroundColor: "#f3f4f6",
+                backgroundColor: theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
                 boxShadow: "none",
                 border: "1px solid rgba(0,0,0,0.05)",
                 textAlign: "left",
                 padding: "1.3rem 2rem",
               }}
               contentArrowStyle={{
-                borderRight: "0.4rem solid #9ca3af"
+                borderRight: theme === "light" ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255,255,255,0.5)"
               }}
               date={experience.date}
               icon={experience.icon}
               iconStyle={{
-                background:"white",
+                background: theme === "light" ? "white" : "rgba(255,255,255,0.15)",
                 fontSize:"1.5rem",
               }}
             >
               <h3 className="font-semibold capitalize">{experience.title}</h3>
               <p className="font-normal !mt-0">{experience.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700">{experience.description}</p>
+              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">{experience.description}</p>
             </VerticalTimelineElement>
           </React.Fragment>
         ))}
