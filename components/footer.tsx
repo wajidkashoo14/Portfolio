f3fab0f004f19dd44c858ee7ca968ccb6466c0e9
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2023-10-13 17:11:58 +0530

    code fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 text-xs block">&copy; 2030 Ricardo. All rights reserved</small>
+      <small className="mb-2 text-xs block">&copy; 2023 Wajid. All rights reserved</small>
       <p className="text-xs">
         <span>
           About this website:
